d138514c98878ded069ee818802481de1bacb94f
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-02-17 21:49:53 +0800

    Functions added
    Add new item function, data structure setup.

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 //budget control
 var budgetController = (function() {
 
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            newItem = new Expense(ID, des, val);
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+            //Return the new element
+            return newItem;
+            }
+        };
 })();
 
 //UI Control
@@ -13,14 +56,14 @@ var UIController = (function() {
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn'
-    }
+    };
 
     return {
             getInput: function() {
             return {
-                var type = document.querySelector(inputType).value, //either inc or exp
-                var description = document.querySelector(inputDescription).value,
-                var value = document.querySelector(inputValue).value,
+                type: document.querySelector(DOMStrings.inputType).value, //either inc or exp
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
             };
         },
 
@@ -34,32 +77,40 @@ var UIController = (function() {
 
 //Global app control
 var controller = (function(budgetCtrl, UICtrl) {
-
+    
     var DOM = UICtrl.getDOMStrings();
+
+    var setupEventListeners = function() {
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+
+        });
+    };
+    
     var ctrlAddItem = function() {
-
+        var input, newItem;
          //1. Get field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         //2. Add the item to the budget controller.
-
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         //3. Add the item to the UI.
 
         //4. Calculate the budget
 
         //5. Display the budget to the UI
 
-    }
+    };
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', function( {
-
-       
-    }))
-
-        document.addEventListener('keypress', function(event) {
-            if(event.keyCode === 13 || event.which === 13) {
-                ctrlAddItem();
-            }
-
-        });
+    return {
+        init: function() {
+            setupEventListeners();
+        }
+    };
 
 })(budgetController, UIController);
+
+controller.init();
