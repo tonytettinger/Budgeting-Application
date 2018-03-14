e0317009c1d9f4ea6af146d7eb25d7594e9aa396
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-03-14 23:21:57 +0800

    Date display implemented

--- app.js
+++ app.js
@@ -151,7 +151,8 @@ var UIController = (function() {
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     var formatNumber = function(num, type) {
@@ -267,6 +268,16 @@ var UIController = (function() {
 
             });  
             
+        },
+
+        displayMonth: function() {
+                var now, month, months, year
+                now = new Date();
+
+                months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+                month = now.getMonth();
+                year = now.getFullYear();
+                document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
         }
     };
 
@@ -359,6 +370,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             setupEventListeners();
+            UICtrl.displayMonth();
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
